abc81966ad803e57d734d2e056fd28e32335b360
Muhammad Usama <usamahameed000@yahoo.com>
2020-02-09 23:58:18 +0500

    Adding all files

---
+++ a.py
@@ -0,0 +1,20 @@
+#def add(a,b):
+#    return a+b
+#def sub(a,b):
+  #  return a-b
+#def muliple(a,b):
+ #   return a*b
+#def division(a,b):
+   # return a/b
+
+def add():
+    print('This is from file 1')
+
+def sub():
+    print('This is from file 2')
+
+def main():
+    add()
+    sub()
+main()
+  
